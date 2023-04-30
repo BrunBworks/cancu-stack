b8cbb8e07e7493e418ddc3be9ac925d1db97ee31
treckstar <treckstar@gmail.com>
2023-04-30 01:54:32 -0400

    fix: selector

--- src/components/commercelayer/Countries.tsx
+++ src/components/commercelayer/Countries.tsx
@@ -18,7 +18,7 @@ const Countries: FunctionComponent<Props> = ({ items, searchBy }) => {
     const countryCode = code.toLowerCase();
     const href = !isEmpty(searchBy)
       ? {
-          pathname: `/[countryCode]/[lang]`,
+          pathname: `/[lang]`,
           query: {
             searchBy,
             countryCode,
@@ -26,7 +26,7 @@ const Countries: FunctionComponent<Props> = ({ items, searchBy }) => {
           },
         }
       : {
-          pathname: `/[countryCode]/[lang]`,
+          pathname: `/[lang]`,
           query: {
             countryCode,
             lang,
@@ -41,8 +41,8 @@ const Countries: FunctionComponent<Props> = ({ items, searchBy }) => {
     );
   });
   return (
-    <div className="bg-white shadow-md p-10 max-w-screen-sm mx-auto rounded">
-      <h1 className="text-xl md:text-2xl mb-8">Choose your country</h1>
+    <div className="max-w-screen-sm p-10 mx-auto bg-white rounded shadow-md">
+      <h1 className="mb-8 text-xl md:text-2xl">Choose your country</h1>
       <div className="grid grid-cols-2 gap-y-14 gap-x-16 md:grid-cols-4 md:gap-y-8 md:gap-x-12">{countries}</div>
     </div>
   );
